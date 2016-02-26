ad58f3d90077862812dc58bbbbe51fd049cb60cc
Mike Cottingham <mcottingham@3esi.com>
2016-02-26 15:19:21 -0700

    Added layout template to page layout

--- app/app.js
+++ app/app.js
@@ -1,32 +1,43 @@
 (function() {
-  'use strict';
+    'use strict';
 
-  angular.module('aquamonitor', ['ui.router', 'aquamonitor.core'])
-    .config(config)
-    .run(run)
-    .constant('_d3', d3);
+    angular.module('aquamonitor', ['ui.router', 'aquamonitor.core'])
+        .config(config)
+        .run(run)
+        .constant('_d3', d3);
 
-  function config($urlRouterProvider, $stateProvider) {
-    $urlRouterProvider.when('/', '/monitor');
+    function config($urlRouterProvider, $stateProvider) {
+        $urlRouterProvider.when('/', '/monitor');
 
         $stateProvider
             .state('root', {
                 url: '/',
                 abstract: true,
-                template: '<ui-view />'
+                templateUrl: 'app/layout/pageLayout.html'
             })
+            .state('root.app', {
+                abstract: true,
+                views: {
+                    'topNav@root': {
+                        templateUrl: 'app/topNav/topNav.html'
+                    }
+                }
+            })
+            .state('root.app.monitor', {
+                url: 'monitor',
+                views: {
+                    'content@root': {
+                        templateUrl: 'app/monitor/monitor.html',
+                        controller: 'monitorCtrl',
+                        controllerAs: 'monitorCtrl'
+                    }
+                }
+            });
+    }
 
-        .state('root.monitor', {
-            url: 'monitor',
-            templateUrl: 'app/monitor/monitor.html',
-            controller: 'monitorCtrl',
-            controllerAs: 'monitorCtrl'
-        })
-  }
+    function run($rootScope, $state) {
+        $rootScope.$state = $state;
+    }
 
-  function run($rootScope, $state) {
-    $rootScope.$state = $state;
-  }
-
-  angular.bootstrap(document, ['aquamonitor']);
+    angular.bootstrap(document, ['aquamonitor']);
 })();
