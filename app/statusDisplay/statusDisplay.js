e700ca69b86ee382b57d326dc20396c4220e719c
Mike Cottingham <mike@mikecottingham.ca>
2016-02-26 00:18:44 -0700

    Draw the status of the pump and lights based on the system status being passed in

--- app/statusDisplay/statusDisplay.js
+++ app/statusDisplay/statusDisplay.js
@@ -9,20 +9,25 @@
     }
 
     function StatusDisplayLink(scope, elem, attr, ngModelCtrl) {
-      angular.element(elem[0]).empty();
-      var svg = _d3.select(elem[0])
-              .append('svg:svg')
-              .append('svg:g');
+      ngModelCtrl.$render = draw;
+
+      function draw() {
+        var systemStatus = ngModelCtrl.$modelValue;
+        angular.element(elem[0]).empty();
+        var svg = _d3.select(elem[0])
+                .append('svg:svg')
+                .append('svg:g');
 
 
-      var defs = svg.insert('svg:defs', 'g');
+        var defs = svg.insert('svg:defs', 'g');
 
-      var aquarium = componentsFactory.getAquarium(svg);
-      var sump = componentsFactory.getSump(svg);
-      var lights = componentsFactory.getLights(svg, true);
-      var pump = componentsFactory.getPump(svg, true);
-      var aquariumReturn = componentsFactory.getAquariumReturn(svg);
-      var sumpReturn = componentsFactory.getSumpReturn(svg);
+        var aquarium = componentsFactory.getAquarium(svg);
+        var sump = componentsFactory.getSump(svg);
+        var lights = componentsFactory.getLights(svg, systemStatus.lights);
+        var pump = componentsFactory.getPump(svg, systemStatus.pump);
+        var aquariumReturn = componentsFactory.getAquariumReturn(svg);
+        var sumpReturn = componentsFactory.getSumpReturn(svg);
+      }
     }
 
     return {
